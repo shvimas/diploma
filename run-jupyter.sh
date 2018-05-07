00f282763ad2768c8de78cd905230a5e2051cb59
shvimas <shvimerd@gmail.com>
2018-05-07 23:37:15 +0300

    remove run-jupiter

--- run-jupyter.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-jupyter notebook --no-browser --ip 127.0.0.1 --port 8888 --port-retries=0 --config='/Users/shvimas/.jupyter/jupyter_notebook_config.py' --notebook-dir='/Users/shvimas/GitHub/diploma' --NotebookApp.token=''
